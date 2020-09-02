cb277c1e1bee2cc77e1b3dffe8438782e4d8c6d4
ketsthechanger <ketanogupta@yahoo.co.uk>
2020-09-02 14:59:05 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source             = "terraform-aws-modules/vpc/aws"
-  name               = "my-vpc"
+  name               = "my-vpc-new"
   cidr               = "10.0.0.0/16"
   azs                = ["us-east-1a"]
   private_subnets    = ["10.0.1.0/24"]
